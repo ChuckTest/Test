bfabab570ebe3dfb90e9804287ba9880b9fb8f4a
chucklu <244657538@qq.com>
2014-07-05 20:58:18 +0800

    1。增加"异步Socket"项目的初始代码

---
+++ AsyncSocket/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+namespace AsyncSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("192.168.1.39"), 1333);
+
+            Server objServer = new Server(1000, 10);
+            objServer.Init();
+            objServer.Start(iep);
+        }
+    }
+}
