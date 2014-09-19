20c46569fa0bc29c13a271cef3fd800cacdd4d6d
ChuckLu <244657538@qq.com>
2014-09-19 15:58:58 +0800

    1.添加画好Grid的2048到版本库

---
+++ 2048/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p=new Pen(Color.Blue);
+            Rectangle r = new Rectangle(0,0,450,450);
+            e.Graphics.DrawRectangle(p, r);
+            r.Height = 100; r.Width = 100;
+            for (int i = 0; i < 4; i++)
+            {
+                r.X = 110 * i + 10;
+                for (int j = 0; j < 4; j++)
+                {
+                    r.Y = 110 * j + 10;
+                    e.Graphics.DrawRectangle(p, r);
+                }
+            }
+        }
+    }
+}
