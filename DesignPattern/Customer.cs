bae37bc2f9e3c26b4381214691bd2c2792bc857a
ChuckLu <244657538@qq.com>
2014-10-20 20:03:31 +0800

    1.完成了非简单工厂的代码，此代码为之后引出简单工厂做铺垫

---
+++ DesignPattern/Customer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    /// <summary>
+    /// 自己做饭的情况
+    /// 没有简单工厂之前，客户想吃什么菜，只能自己炒
+    /// </summary>
+    class Customer
+    {
+        public static Food Cook(string type)
+        {
+            Food food = null;
+            if (type.Equals("西红柿炒蛋"))
+            {
+                food = new TomatoScrambledEggs();
+            }
+            else if (type.Equals("红烧鱼"))
+            {
+                food = new Fish();
+            }
+            return food;
+        }
+
+        static void Main(string[] args)
+        {
+            Food food1 = Cook("西红柿炒蛋");
+            food1.Print();
+
+            Food food2 = Cook("红烧鱼");
+            food2.Print();
+
+            Console.Read();
+        }
+    }
+
+    /// <summary>
+    /// 菜的抽象类
+    /// </summary>
+    public abstract class Food
+    {
+        /// <summary>
+        /// 输出点了什么菜
+        /// </summary>
+        public abstract void Print();
+    }
+
+    /// <summary>
+    /// 西红柿炒蛋这道菜
+    /// </summary>
+    public class TomatoScrambledEggs : Food
+    {
+        public override void Print()
+        {
+            Console.WriteLine("一份西红柿炒鸡蛋");
+        }
+    }
+
+    /// <summary>
+    /// 红烧鱼这道菜
+    /// </summary>
+    public class Fish : Food
+    {
+        public override void Print()
+        {
+            Console.WriteLine("一份红烧鱼");
+        }
+    }
+}
