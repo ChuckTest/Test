e86f9c2a386a2fe1218452cb0fba941647ead516
ChuckLu <244657538@qq.com>
2014-10-23 21:09:33 +0800

    1.添加"多用途事件处理程序"的部分代码

---
+++ CommonTest/Chapter13/Ch13Ex03/Ch13Ex03.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonTest.Chapter13.Ch13Ex03
+{
+    class Ch13Ex03
+    {
+    }
+}
---
+++ CommonTest/Chapter13/Ch13Ex03/MessageArrivedEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonTest.Chapter13.Ch13Ex03
+{
+    class MessageArrivedEventArgs : EventArgs
+    {
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+        }
+
+        public MessageArrivedEventArgs()
+        {
+            message = "No message sent.";
+        }
+
+        public MessageArrivedEventArgs(string newMessage)
+        {
+            message = newMessage;
+        }
+    }
+}
