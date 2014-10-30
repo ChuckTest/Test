172b6a4b97375149d5aa8bf83bbd5a0717fb7dbc
ChuckLu <244657538@qq.com>
2014-10-30 18:49:30 +0800

    1.修改高效率导出excel的代码

---
+++ CommonTest/Exccel/ExcelHelper.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonTest.Log;
+using Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+using DataTable = System.Data.DataTable;
+namespace CommonTest.Exccel
+{
+    class ExcelHelper
+    {
+        private Application app;//excel应用
+        private Workbook workBook;//工作簿
+        private Worksheet currentWorkSheet;//工作表
+        private Range range;//单元格区域
+        private object missing = Missing.Value;
+        /// <summary>
+        /// 导出的Excel的文件保存路径
+        /// </summary>
+        private string outputFile;
+        private int sheetCount = 1;   //WorkSheet数量
+
+        private int rows = 30000;//默认每一个工作表的数量是30000
+
+        public int RowsCountEveryWorkSheet
+        {
+            set { rows = value; }
+        }
+
+        /// <summary>
+        /// 不允许外部调用无参构造函数
+        /// </summary>
+        private ExcelHelper() { }
+
+        /// <summary>
+        /// 构造函数，新建一个工作簿
+        /// </summary>
+        public ExcelHelper(string filePath)
+        {
+            app = new ApplicationClass() { Visible = false };//新建一个Excel的App
+            workBook = app.Workbooks.Add(Type.Missing);//新建一个工作簿
+            currentWorkSheet = (Worksheet)workBook.Sheets.get_Item(1);//让currentWorkSheet指向工作簿中的第一个工作表
+            outputFile = filePath;//获取保存文件的路径
+        }
+
+        /// <summary>
+        /// 给工作簿加一个新的工作表，并使currentWorkSheet指向新表
+        /// </summary>
+        public void CreateWorkSheet()
+        {
+            Worksheet lastWorkSheet = (Worksheet)workBook.Sheets.get_Item(workBook.Sheets.Count);//获取工作簿中最后一个工作表
+            currentWorkSheet = (Worksheet)workBook.Sheets.Add(missing, lastWorkSheet, 1, missing);//新增一个工作表,并将新表赋给currentWorkSheet
+        }
+
+        /// <summary>
+        /// 将DataTable数据写入Excel文件（自动分页）
+        /// </summary>
+        /// <param name="dt">DataTable</param>
+        /// <param name="rows">每个WorkSheet写入多少行数据</param>
+        public void DataTableToExcel(DataTable dt)
+        {
+            int startRow;//记录起始行索引 
+            int endRow;//记录结束行索引
+            int row;//当前需要写入worksheet的数据行数
+            int rowCount = dt.Rows.Count; //DataTable行数
+            int colCount = dt.Columns.Count; //DataTable列数
+            sheetCount = GetSheetCount(rowCount, rows); //WorkSheet个数
+
+            //复制sheetCount-1个WorkSheet对象
+            if (sheetCount > 1)
+            {
+                workBook.Sheets.Add(missing, currentWorkSheet, sheetCount - 1, missing);
+            }
+
+            int initialIndex = currentWorkSheet.Index;
+            for (int i = 0; i < sheetCount; i++)
+            {
+                startRow = i * rows;
+                if (i == sheetCount - 1)//若是最后一个WorkSheet，那么记录结束行索引为源DataTable行数
+                {
+                    endRow = rowCount - 1;
+                }
+                else
+                {
+                    endRow = (i + 1) * rows - 1;
+                }
+
+                //获取要写入数据的WorkSheet对象，并重命名
+                currentWorkSheet = (Worksheet)workBook.Worksheets.get_Item(initialIndex + i);
+                if (sheetCount > 1)
+                {
+                    currentWorkSheet.Name = String.Format("{0}-{1}", dt.TableName, i + 1);
+                }
+                else
+                {
+                    currentWorkSheet.Name = dt.TableName;
+                }
+
+                row = endRow - startRow + 1;
+                object[,] ss = new object[row + 1, colCount];
+
+                for (int j = 0; j <= row; j++)
+                {
+                    for (int k = 0; k < colCount; k++)
+                    {
+                        if (j == 0)
+                        {
+                            ss[j, k] = dt.Columns[k].ColumnName;
+                        }
+                        else
+                        {
+                            if (dt.Columns[k].ColumnName.Equals("时间"))
+                            {
+                                ss[j, k] = Convert.ToDateTime(dt.Rows[startRow + j - 1][k]).ToString("s").Replace('T', '/');
+                            }
+                            else
+                            {
+                                ss[j, k] = dt.Rows[startRow + j - 1][k];
+                            }
+                        }
+                    }
+                }
+
+                range = (Range)currentWorkSheet.Cells[1, 1];
+                range = range.get_Resize(row + 1, colCount);
+                range.Value2 = ss;
+                range.EntireColumn.AutoFit();
+                range.VerticalAlignment = XlVAlign.xlVAlignBottom;
+                range.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            }
+        }
+
+        /// <summary>
+        /// 计算WorkSheet数量
+        /// </summary>
+        /// <param name="rowCount">记录总行数</param>
+        /// <param name="rows">每WorkSheet行数</param>
+        public static int GetSheetCount(int rowCount, int rows)
+        {
+            return Convert.ToInt32(Math.Ceiling(Convert.ToDouble(rowCount) / Convert.ToDouble(rows)));//两数相除，然后向上取整
+        }
+
+        /// <summary>
+        /// 根据名称删除工作表
+        /// </summary>
+        /// <param name="sheetName"></param>
+        public void DeleteWorkSheet(string sheetName)
+        {
+            try
+            {
+                for (int i = 1; i <= workBook.Sheets.Count; i++)//遍历工作簿中的所有工作表
+                {
+                    currentWorkSheet = (Worksheet)workBook.Sheets.get_Item(i);
+                    if (currentWorkSheet.Name.IndexOf(sheetName) >= 0)//如果工作表的表明包含sheetName
+                    {
+                        currentWorkSheet.Delete();//删除
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionLog.Instance.WriteLog(ex, LogType.UI);
+                Dispose();
+                throw ex;
+            }
+        }
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
+        /// <summary>
+        /// Kill掉之前创建的excel进程
+        /// </summary>
+        /// <param name="appHwnd"></param>
+        private static void KillExcelProcess(int appHwnd)
+        {
+            Process[] ps = Process.GetProcesses();
+            IntPtr t = new IntPtr(appHwnd); //得到这个句柄，具体作用是得到这块内存入口 
+            int ExcelID = 0;
+            GetWindowThreadProcessId(t, out ExcelID); //得到本进程唯一标志
+            foreach (Process p in ps)
+            {
+                if (p.ProcessName.ToLower().Equals("excel"))
+                {
+                    if (p.Id == ExcelID)
+                    {
+                        p.Kill();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 保存文件
+        /// </summary>
+        public void SaveAsFile()
+        {
+            if (outputFile == null)
+            {
+                throw new Exception("没有指定输出文件路径！");
+            }
+
+            XlFileFormat fileFormat;
+            if (String.Compare(Path.GetExtension(outputFile).ToLower(), ".xlsx", false) == 0)
+            {
+                fileFormat = XlFileFormat.xlWorkbookDefault; //Excel 2007版本 
+            }
+            else
+            {
+                fileFormat = XlFileFormat.xlAddIn8;//Excel 2003版本 
+            }
+
+            try
+            {
+                workBook.SaveAs(outputFile, fileFormat, missing, missing, missing, missing, XlSaveAsAccessMode.xlExclusive, missing, missing, missing, missing, missing);
+            }
+            catch (Exception ex)
+            {
+                ExceptionLog.Instance.WriteLog(ex, LogType.UI);
+                throw;
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        private void Dispose()
+        {
+            int appHwnd = 0;
+            try
+            {
+                if (currentWorkSheet != null)
+                {
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(currentWorkSheet);
+                    currentWorkSheet = null;
+                }
+                if (workBook != null)
+                {
+                    workBook.Close(true, missing, missing);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workBook);
+                    workBook = null;
+                }
+                if (app != null)
+                {
+                    appHwnd = app.Hwnd;
+                    app.Workbooks.Close();
+                    app.Quit();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+                    app = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionLog.Instance.WriteLog(ex, LogType.UI);
+                throw ex;
+            }
+            GC.Collect();
+            if (appHwnd > 0)
+            {
+                KillExcelProcess(appHwnd);
+            }
+        }
+    }
+}
--- CommonTest/Exccel/ExcelOperation.cs
+++ CommonTest/Exccel/ExcelOperation.cs
@@ -9,338 +9,144 @@ using Microsoft.Office.Interop.Excel;
 using System.IO;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using CommonTest.Log;
 namespace CommonTest.Exccel
 {
     public class ExcelOperation
     {
-        private static object missing = Missing.Value;//或者missing = System.Type.Missing;两者等价
 
-        private static Application app;
-
-        /// <summary>
-        /// 工作簿
-        /// </summary>
-        private static Workbook workBook;
+        private ExcelOperation()
+        { }
 
         /// <summary>
-        /// 工作表
+        /// 导出数据表
         /// </summary>
-        private static Worksheet workSheet;
-
-        /// <summary>
-        /// 导入数据表时，当前数据表所需要的worksheet的数量
-        /// </summary>
-        private static int sheetCount = 1;
-
-        /// <summary>
-        /// 导入数据集时，假如正在导入第3个数据表，那么totalSheetCount是这3张表所需要的worksheet的数量
-        /// </summary>
-        private static int totalSheetCount = 0;
-
-        /// <summary>
-        /// 导入数据集时，假如正在导入第3个数据表，那么totalSheetCount是前2张表所需要的worksheet的数量
-        /// </summary>
-        private static int preTotalWorkSheetCount = 0;//记录之前数据表的worsheet的数量
-
-        /// <summary>
-        /// 每个WorkSheet允许的行数
-        /// </summary>
-        private static int EverySheetRows = 3 * 100 * 100;
-
-
-        /// <summary>
-        /// 导出一个数据表的数据
-        /// </summary>
-        /// <param name="dataTable">数据表</param>
-        /// <param name="filePath">包含文件名的文件路径</param>
-        public static void ExportExcel(DataTable dataTable, string filePath)
+        /// <param name="dt">数据表</param>
+        /// <param name="filePath">文件路径</param>
+        /// <returns></returns>
+        public static bool ExportExcel(DataTable dt, string filePath)
         {
             try
             {
-                Init();
-                DataTableToExcel(dataTable, EverySheetRows);
-                SaveAs(filePath);
+#if Debug
+                Console.WriteLine(Environment.NewLine + "单表导出开始new  " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+#endif
+                ExcelHelper Excel = new ExcelHelper(filePath);
+                Excel.CreateWorkSheet();
+                Excel.DataTableToExcel(dt);
+                Excel.DeleteWorkSheet("Sheet");
+                Excel.SaveAsFile();
+#if Debug
+                Console.WriteLine("单表导出结束new  {0}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+#endif
+                return true;
             }
             catch (Exception ex)
             {
-                throw ex;
-            }
-        }
-
-        /// <summary>
-        /// 导出多个数据表的数据
-        /// </summary>
-        /// <param name="dataSet">数据集(包含多个数据表)</param>
-        /// <param name="filePath">包含文件名的文件路径</param>
-        public static void ExportExcel(DataSet dataSet, string filePath)
-        {
-            try
-            {
-                Init();
-                for (int i = 0; i < dataSet.Tables.Count; i++)
-                {
-                    DataTableToExcel(dataSet.Tables[i], EverySheetRows);
-                }
-                SaveAs(filePath);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        /// <summary>
-        /// 清空工作簿上的工作表  但是会剩余一个[工作簿上至少要保留一个工作表]
-        /// </summary>
-        private static void ClearSheet()
-        {
-            try
-            {
-                int count = workBook.Sheets.Count;
-                for (int i = 0; i < count - 1; i++)//此循环仅仅代表循环次数
-                {
-                    DeleteWorkSheet(workBook.Sheets.Count);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        /// <summary>
-        /// 初始化app,工作簿和工作表
-        /// </summary>
-        private static void Init()
-        {
-            app = new Application();
-            app.Visible = false;
-            app.DisplayAlerts = false;//覆盖非空单元格的时候，不提示
-            app.AlertBeforeOverwriting = false;//另存为时，覆盖同名文件不提示
-            workBook = app.Workbooks.Add(missing);
-            ClearSheet();
-        }
-
-        /// <summary>
-        /// 另存为   保存完之后，自动释放资源
-        /// </summary>
-        /// <param name="filePath"></param>
-        private static void SaveAs(string filePath)
-        {
-            workSheet = workBook.Sheets.get_Item(1);//设置当前的工作表为第一个WorkSheet  [不需要判断，第一个WorkSheet必然存在]
-            //必须转换一下，否则会提示Microsoft.Office.Interop.Excel.._Worksheet.Active()和Microsoft.Office.Interop.Excel.DocEvents_Event.Active之间存在二义性,将使用方法组.
-            ((_Worksheet)workSheet).Activate();//确保使用Excel打开文件之后，显示的是第一个
-
-            if (filePath == null)
-            {
-                return;
-            }
-            XlFileFormat fileFormat;
-            if (Path.GetExtension(filePath).Equals(".xlsx"))
-            {
-                fileFormat = XlFileFormat.xlWorkbookDefault; //Excel 2007版本 
-            }
-            else
-            {
-                throw new Exception("文件格式错误");
-            }
-            try
-            {
-                workBook.SaveAs(filePath, fileFormat, missing, missing, missing, missing, XlSaveAsAccessMode.xlExclusive, missing, missing, missing, missing, missing);
-            }
-            catch
-            {
+                ExceptionLog.Instance.WriteLog(ex, LogType.UI);
                 throw;
             }
-            finally
-            {
-                Dispose();
-            }
         }
 
         /// <summary>
-        /// 将DataTable导出到Excel
+        /// 导出数据表
         /// </summary>
-        /// <param name="table">数据表</param>
-        /// <param name="rowsPerSheet">每rowsPerSheet个数据分一页</param>
-        private static void DataTableToExcel(DataTable table, int rowsPerSheet)
+        /// <param name="dt">数据表</param>
+        /// <param name="filePath">文件路径</param>
+        /// <param name="rowsCountEveryWorkSheet">worksheet允许的行数</param>
+        /// <returns></returns>
+        public static bool ExportExcel(DataTable dt, string filePath, int rowsCountEveryWorkSheet)
         {
-            int i = 0;//工作表的索引
-            int j = 0;//table的行索引
-            int k = 0;//列索引
             try
             {
-                if (table.TableName.Equals(string.Empty))
-                {
-                    throw new Exception("数据表的表名不能为空");
-                }
-                if (rowsPerSheet <= 0 || rowsPerSheet > 65535)
-                {
-                    throw new Exception("工作表所允许的数据行必须大于0并且小于等于65535");
-                }
-                if (table.Columns.Count > 255)
-                {
-                    throw new Exception("数据表的列数必须小于等于255");
-                }
-                int rowsCount = table.Rows.Count;//当前表的数据行数
-                sheetCount = CalculateSheetCount(rowsCount, rowsPerSheet);
-                preTotalWorkSheetCount = totalSheetCount;
-                totalSheetCount = totalSheetCount + sheetCount;
-                if (sheetCount + workBook.Sheets.Count > 255)
-                {
-                    throw new Exception("数据量太大,请减少行数");
-                }
-                InitWorkSheet();
-                i = workSheet.Index;
-                for (; i <= workBook.Sheets.Count; i++)//遍历之前生成的工作表，进行数据的赋值
-                {
-                    workSheet = workBook.Sheets.get_Item(i);
-                    if (sheetCount > 1)
-                    {
-                        workSheet.Name = string.Format("{0}-{1}", table.TableName, i - preTotalWorkSheetCount);
-                    }
-                    else
-                    {
-                        workSheet.Name = table.TableName;
-                    }
-                    j = 0;
-                    for (; j < rowsPerSheet; j++)//遍历行
-                    {
-                        if ((i - 1 - preTotalWorkSheetCount) * rowsPerSheet + j < rowsCount)//确保数据(i-1) * rowsPerSheet+j不会超出Table表的行数
-                        {
-                            k = 0;
-                            for (; k < table.Columns.Count; k++)//遍历列
-                            {
-                                workSheet.Cells[k + 1][j + 1] = table.Rows[j + (i - 1 - preTotalWorkSheetCount) * rowsPerSheet][k].ToString();//需要注意的是,Excel的单元格索引,是从1开始的
-                            }
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
+#if Debug
+                Console.WriteLine(Environment.NewLine + "单表导出开始new  " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+#endif
+                ExcelHelper Excel = new ExcelHelper(filePath);
+                Excel.RowsCountEveryWorkSheet = rowsCountEveryWorkSheet;
+                Excel.CreateWorkSheet();
+                Excel.DataTableToExcel(dt);
+                Excel.DeleteWorkSheet("Sheet");
+                Excel.SaveAsFile();
+#if Debug
+                Console.WriteLine("单表导出结束new  {0}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+#endif
+                return true;
             }
             catch (Exception ex)
             {
-                Dispose();
-                throw ex;
+                ExceptionLog.Instance.WriteLog(ex, LogType.UI);
+                throw;
             }
         }
 
         /// <summary>
-        /// 初始化工作表(根据需要的工作表的数量来添加或者移除工作表)
+        /// 导出数据集
         /// </summary>
-        /// <param name="sheetCount"></param>
-        private static void InitWorkSheet()
-        {
-            if (sheetCount <= 0)
-            {
-                throw new Exception("工作表的数量错误");
-            }
-            if (totalSheetCount == 1)//如果需要的工作表的数量仅仅是1的话，那么默认的第一张表就可以拿来直接使用
-            {
-                workSheet = workBook.Sheets.get_Item(1);
-            }
-            else
-            {
-                workSheet = workBook.Sheets.get_Item(workBook.Sheets.Count);//获取当前工作簿中最后一张工作表
-                workBook.Sheets.Add(missing, workSheet, totalSheetCount - workBook.Sheets.Count, missing);//在当前最后一张工作表后新增totalSheetCount - workBook.Sheets.Count工作表
-                workSheet = workBook.Sheets.get_Item(totalSheetCount - sheetCount + 1);//workSheet设置为新增的第一张工作表
-            }
-        }
-
-        /// <summary>
-        /// 根据索引删除工作表
-        /// </summary>
-        /// <param name="sheetIndex"></param>
-        private static void DeleteWorkSheet(int sheetIndex)
-        {
-            try
-            {
-                if (sheetIndex > workBook.Sheets.Count)
-                {
-                    throw new Exception("索引超出范围，WorkSheet索引不能大于WorkSheet数量！");
-                }
-                Worksheet sheet = null;
-                sheet = (Worksheet)workBook.Sheets.get_Item(sheetIndex);
-                sheet.Delete();
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-        }
-
-        /// <summary>
-        /// 计算需要的工作表的数量
-        /// </summary>
-        /// <param name="totalRows"></param>
-        /// <param name="sheetRows"></param>
+        /// <param name="ds">数据集</param>
+        /// <param name="filePath">文件路径</param>
         /// <returns></returns>
-        private static int CalculateSheetCount(int totalRows, int sheetRows)
+        public static bool ExportExcel(DataSet ds, string filePath)
         {
-            double result = Convert.ToDouble(totalRows) / Convert.ToDouble(sheetRows);
-            return Convert.ToInt32(Math.Ceiling(result));//向上取整
-        }
-
-        /// <summary>
-        /// 释放资源
-        /// </summary>
-        private static void Dispose()
-        {
-            int appHwnd = app.Hwnd;
             try
             {
-                workBook.Close(true, missing, missing);
-                app.Workbooks.Close();
-                app.Quit();
-                if (workSheet != null)
+#if Debug
+                Console.WriteLine(Environment.NewLine + "多表导出开始new  " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+#endif
+                if (ds == null || ds.Tables.Count == 0) return false;
+                ExcelHelper Excel = new ExcelHelper(filePath);
+                foreach (DataTable dt in ds.Tables)
                 {
-                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workSheet);
-                    workSheet = null;
+                    Excel.CreateWorkSheet();
+                    Excel.DataTableToExcel(dt);
                 }
-                if (workBook != null)
-                {
-                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workBook);
-                    workBook = null;
-                }
-                if (app != null)
-                {
-                    System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
-                    app = null;
-                }
+                Excel.DeleteWorkSheet("Sheet");
+                Excel.SaveAsFile();
+#if Debug
+                Console.WriteLine("多表导出结束new  {0}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+#endif
+                return true;
             }
-            catch
+            catch (Exception ex)
             {
-
+                ExceptionLog.Instance.WriteLog(ex, LogType.UI);
+                throw;
             }
-            GC.Collect();
-            KillExcelProcess(appHwnd);
-
         }
 
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
-        public static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
         /// <summary>
-        /// Kill掉之前创建的excel进程
+        /// 导出数据集
         /// </summary>
-        /// <param name="appHwnd"></param>
-        private static void KillExcelProcess(int appHwnd)
+        /// <param name="ds">数据集</param>
+        /// <param name="filePath">文件路径</param>
+        /// <param name="rowsCountEveryWorkSheet">worksheet允许的行数</param>
+        /// <returns></returns>
+        public static bool ExportExcel(DataSet ds, string filePath, int rowsCountEveryWorkSheet)
         {
-            Process[] ps = Process.GetProcesses();
-            IntPtr t = new IntPtr(appHwnd); //得到这个句柄，具体作用是得到这块内存入口 
-            int ExcelID = 0;
-            GetWindowThreadProcessId(t, out ExcelID); //得到本进程唯一标志k
-            foreach (Process p in ps)
+            try
             {
-                if (p.ProcessName.ToLower().Equals("excel"))
+#if Debug
+                Console.WriteLine(Environment.NewLine + "多表导出开始new  " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+#endif
+                if (ds == null || ds.Tables.Count == 0) return false;
+                ExcelHelper Excel = new ExcelHelper(filePath);
+                Excel.RowsCountEveryWorkSheet = rowsCountEveryWorkSheet;
+                foreach (DataTable dt in ds.Tables)
                 {
-                    if (p.Id == ExcelID)
-                    {
-                        p.Kill();
-                    }
+                    Excel.CreateWorkSheet();
+                    Excel.DataTableToExcel(dt);
                 }
+                Excel.DeleteWorkSheet("Sheet");
+                Excel.SaveAsFile();
+#if Debug
+                Console.WriteLine("多表导出结束new  {0}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+#endif
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ExceptionLog.Instance.WriteLog(ex, LogType.UI);
+                throw;
             }
         }
     }
