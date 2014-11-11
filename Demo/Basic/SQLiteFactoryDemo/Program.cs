6ff0e974c389fbbb9bc512e06d180c4668219ace
ChuckLu <244657538@qq.com>
2014-11-11 22:21:51 +0800

    1.添加了从SQLite数据库读取数据的代码

---
+++ Demo/Basic/SQLiteFactoryDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Configuration;
+
+namespace SQLiteFactoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["sqlite"].ConnectionString; ;
+
+            //    ConnectionStringSettings connectionStringSettings = ConfigurationManager.ConnectionStrings["SQLite Data Provider"];
+
+            DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.SQLite");
+
+            DbConnectionStringBuilder csb = factory.CreateConnectionStringBuilder();
+            if (csb != null)
+            {
+                csb.ConnectionString = connStr;
+                if (csb.ContainsKey("MultipleActiveResultSets"))//如果支持MultipleActiveResultSets就设置对应的属性
+                {
+                    csb["MultipleActiveResultSets"] = true;
+                }
+                connStr = csb.ConnectionString;
+            }
+
+            DbConnection conn = factory.CreateConnection();
+            conn.ConnectionString = connStr;
+            conn.Open();
+
+
+            DbCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from channeltable";
+            cmd.CommandType = CommandType.Text;
+
+            DataTable table;
+            DataSet ds = new DataSet();
+            SQLiteConnection sqlconn = new SQLiteConnection(conn.ConnectionString);
+            using (SQLiteDataAdapter sda = new SQLiteDataAdapter(new SQLiteCommand(cmd.CommandText, sqlconn)))
+            {
+                sda.Fill(ds);
+            }
+            table = ds.Tables[0];
+            Console.WriteLine(table.Rows.Count);
+            conn.Close();
+            Console.Read();
+        }
+    }
+}
