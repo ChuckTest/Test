1d45055a6d843ba219a1206f7a3fb56368abb882
ChuckLu <244657538@qq.com>
2014-11-13 09:46:45 +0800

    1.增加同步程序的处理，测试是否是同一个线程，是否是线程池中的程序

---
+++ DotNet4.5开发指南/并行处理和并发/异步编程模式/APM/SyncProcedure/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+namespace SyncProcedure
+{
+    //同步程序的试验
+    class Program
+    {
+        //平常写的Hello 程序是同一个线程的,而且不是线程池理的线程程序.
+        static void Main(string[] args)
+        {
+            // 查看当前的线程ID, 是否线程池里面的线程
+            Console.WriteLine("1,Thread ID:#{0},Is PoolThread?{1}",
+Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+
+            SyncTest test = new SyncTest();
+            string val = test.Hello("Andy Huang");
+            Console.WriteLine(val);
+
+            Console.ReadLine();
+
+            //1,Thread ID:#9,Is PoolThread?False
+            //2,Thread ID:#9,Is PoolThread?False
+            //Hello:Andy Huang
+        }
+    }
+}
---
+++ DotNet4.5开发指南/并行处理和并发/异步编程模式/APM/SyncProcedure/SyncTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+namespace SyncProcedure
+{
+    class SyncTest
+    {
+        public string Hello(string name)
+        {        
+            // 查看当前的线程ID, 是否线程池里面的线程
+            Console.WriteLine("2,Thread ID:#{0},Is PoolThread?{1}",
+  Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            return "Hello:" + name;
+        }
+    }
+}
