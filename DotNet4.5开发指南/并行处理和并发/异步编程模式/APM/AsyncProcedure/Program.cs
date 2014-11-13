c2591062c6ce08205a4408c40fc5e0b53fba1c7e
ChuckLu <244657538@qq.com>
2014-11-13 20:54:26 +0800

    1.增加了使用委托异步的代码

---
+++ DotNet4.5开发指南/并行处理和并发/异步编程模式/APM/AsyncProcedure/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace AsyncProcedure
+{
+    public delegate string AsyncEventHandler(string name);
+    
+    class AsyncTest
+    {
+        public string Hello(string name)
+        {
+            // 查看当前的线程ID, 是否线程池里面的线程
+            Console.WriteLine("2,Thread ID:#{0},Is PoolThread?{1}",Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            return "Hello:" + name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            // 查看当前的线程ID, 是否线程池里面的线程
+            Console.WriteLine("1,Thread ID:#{0},Is PoolThread?{1}", Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+           
+            AsyncTest test = new AsyncTest();
+
+            //把Hello 方法分配给委托对象
+            AsyncEventHandler async = test.Hello; //
+
+            //发起一个异步调用的方法,赋值"Andy Huang", 返回IAsyncResult 对象
+            IAsyncResult result = async.BeginInvoke("Andy Huang", null, null);
+
+            //这里会阻碍线程,直到方法执行完毕
+            string val = async.EndInvoke(result);
+
+            Console.WriteLine(val);
+            Console.ReadLine(); // 让黑屏等待,不会直接关闭..
+        }
+    }
+}
