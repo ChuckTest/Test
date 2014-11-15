3cd7c55f1f06a9af9a076219d0e86490cdeb7cf1
ChuckLu <244657538@qq.com>
2014-11-15 20:32:19 +0800

    1.APM中增加了三个使用IAsyncResult设计模式实现的异步 2.其中第一个直接调用End方法阻塞主线程，第二个调用AsyncWaitHandle的WaitOne方法来阻塞主线程，另外一个轮询异步执行是否成功

---
+++ DotNet4.5开发指南/并行处理和并发/异步编程模式/APM/使用IAsyncResult调用异步方法/AsyncWaitHandle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+
+namespace AsyncWaitHandle
+{
+    /// <summary>
+    /// 使用 AsyncWaitHandle 阻止应用程序的执行
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 通过调用IAsyncResult中AsyncWaitHandle的WaitOne方法来阻塞主线程
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string str = "www.baidu.com";
+            IAsyncResult result = Dns.BeginGetHostEntry(str, null, null);//不使用AsyncCallBack委托，也不传递应用程序的状态
+            //这边可以做其他的事情
+            Console.WriteLine("在调用EndGetHostEntry方法前还可以处理其他事情");
+            result.AsyncWaitHandle.WaitOne();//不加等待的时间，就一直阻塞,直到BeginGetHostEntry完成
+            try
+            {
+                //获取IP主机入口
+                IPHostEntry h = Dns.EndGetHostEntry(result);//如果BeginGetHostEntry没有完成的话，这边会阻塞
+                for (int i = 0; i < h.Aliases.Length; i++)
+                {
+                    Console.WriteLine("别名{0}：{1}", i, h.Aliases[i]);
+                }
+                for (int i = 0; i < h.AddressList.Length; i++)
+                {
+                    Console.WriteLine("IP{0}：{1}", i, h.AddressList[i].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ DotNet4.5开发指南/并行处理和并发/异步编程模式/APM/使用IAsyncResult调用异步方法/Poll/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+
+namespace Poll
+{
+    /// <summary>
+    /// 轮询异步操作的状态
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "www.baidu.com";
+            IAsyncResult result = Dns.BeginGetHostEntry(str, null, null);//不使用AsyncCallBack委托，也不传递应用程序的状态
+            //这边可以做其他的事情
+            Console.WriteLine("在调用EndGetHostEntry方法前还可以处理其他事情");
+            
+            int count = 0;
+            //轮询异步操作是否完成的状态
+            while (result.IsCompleted == false)//如果异操作没有完成的话
+            {
+                if ((count + 1) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+                count++;
+                Console.Write(".");
+            }
+            Console.WriteLine();
+
+            try
+            {
+                //获取IP主机入口
+                IPHostEntry h = Dns.EndGetHostEntry(result);//如果BeginGetHostEntry没有完成的话，这边会阻塞
+                for (int i = 0; i < h.Aliases.Length; i++)
+                {
+                    Console.WriteLine("别名{0}：{1}", i, h.Aliases[i]);
+                }
+                for (int i = 0; i < h.AddressList.Length; i++)
+                {
+                    Console.WriteLine("IP{0}：{1}", i, h.AddressList[i].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
