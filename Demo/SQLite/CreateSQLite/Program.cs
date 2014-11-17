bf3feb498cfa5c7977d4e8969cf2af6af025ce01
ChuckLu <244657538@qq.com>
2014-11-17 17:41:51 +0800

    1.增加了使用System.Data.SQLite.dll的说明文档 2.增加了使用此dll创建数据库的代码

---
+++ Demo/SQLite/CreateSQLite/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+
+namespace CreateSQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filePath = Environment.CurrentDirectory;
+                string fileName = string.Format("{0}.db3",DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+                string str = filePath + "\\" + fileName;
+                SQLiteConnection.CreateFile(str);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
