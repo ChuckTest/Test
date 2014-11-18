28f5170787c40f1d394f9338c2c7270f1563aefa
ChuckLu <244657538@qq.com>
2014-11-18 20:43:56 +0800

    1.增加了DateTime的demo

---
+++ DotNetFrameworkClassLibrary/System/DateTime/ToStringDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ToStringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            DateTime utcNow = now.ToUniversalTime();
+            string time = string.Empty;
+
+            time = now.ToString("s");
+            Console.WriteLine(time);
+
+            DateTime datetime = Convert.ToDateTime(time);
+            time = datetime.ToString("s");
+            Console.WriteLine(time);
+
+            time = utcNow.ToString("s");
+            Console.WriteLine(time);
+
+            datetime = Convert.ToDateTime(time);
+            time = datetime.ToString("s");
+            Console.WriteLine(time);
+
+            Console.WriteLine(System.Threading.Thread.CurrentThread.CurrentCulture.Name);
+
+            Console.WriteLine(DateTimeFormatInfo.CurrentInfo);//这个是重点
+
+
+            Console.ReadLine();
+        }
+    }
+}
