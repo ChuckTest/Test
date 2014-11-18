eba9a82be5b31d3dd43c46c00cce06085bf2ed6d
ChuckLu <244657538@qq.com>
2014-11-18 20:43:31 +0800

    1.创建数据库并加密

--- Demo/SQLite/CreateSQLite/Program.cs
+++ Demo/SQLite/CreateSQLite/Program.cs
@@ -10,20 +10,45 @@ namespace CreateSQLite
 {
     class Program
     {
+        static string str;
+        static string passwd = "zbm";
         static void Main(string[] args)
+        {
+            CreateSQLiteDB();
+            Console.ReadLine();
+        }
+
+        static void CreateSQLiteDB()
         {
             try
             {
                 string filePath = Environment.CurrentDirectory;
-                string fileName = string.Format("{0}.db3",DateTime.Now.ToString("yyyyMMdd_HHmmss"));
-                string str = filePath + "\\" + fileName;
+                string fileName = string.Format("{0}.db3", DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+                str = filePath + "\\" + fileName;
                 SQLiteConnection.CreateFile(str);
+                SetPassword();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            Console.ReadLine();
+        }
+
+        static void SetPassword()
+        {
+            SQLiteConnectionStringBuilder s = new SQLiteConnectionStringBuilder();
+            s.DataSource = str;
+            s.Password = passwd;
+
+            //SQLiteConnection conn = new SQLiteConnection(s.ToString());
+            //conn.Open();
+            //conn.ChangePassword(passwd);
+            //conn.Close();
+
+            SQLiteConnection conn = new SQLiteConnection(s.ToString());
+            conn.Open();
+            conn.ChangePassword(passwd);
+            conn.Close();
         }
     }
 }
