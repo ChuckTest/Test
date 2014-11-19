e102d631ab78da3505a7e8c6801eacb3aed5fb75
ChuckLu <244657538@qq.com>
2014-11-19 21:04:25 +0800

    1.增加了使用委托进行异步编程的，EndInvoke代码示例

---
+++ DotNet4.5开发指南/并行处理和并发/异步编程模式/APM/使用委托进行异步编程/AsyncDemo/AsyncDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncDemoTest
+{
+    /// <summary>
+    /// 定义一个委托
+    /// </summary>
+    /// <param name="callDuration"></param>
+    /// <param name="threadId"></param>
+    /// <returns></returns>
+    public delegate string AsyncMethodCaller(int callDuration,out int threadId);
+
+    /// <summary>
+    /// 定义一个类
+    /// </summary>
+    public class AsyncDemo
+    {
+        public string TestMethod(int callDuration, out int threadId)
+        {
+            Console.WriteLine("TestMethod begins.");
+            Thread.Sleep(callDuration);
+            threadId = Thread.CurrentThread.ManagedThreadId;
+            return string.Format("My call time is {0}", callDuration);
+        }
+    }
+}
---
+++ DotNet4.5开发指南/并行处理和并发/异步编程模式/APM/使用委托进行异步编程/EndInvokeDemo/AsyncMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using AsyncDemoTest;
+
+namespace EndInvokeDemo
+{
+    public class AsyncMain
+    {
+        public static void Main()
+        {
+            int threadId;
+
+            AsyncDemo ad = new AsyncDemo();//创建一个类的实例
+
+            AsyncMethodCaller caller = new AsyncMethodCaller(ad.TestMethod);//创建一个委托的实例，并将类的方法作为参数传递
+
+            IAsyncResult result = caller.BeginInvoke(3000, out threadId, null, null);//异步调用
+
+            Thread.Sleep(0);//线程被阻塞的毫秒数。 指定零 (0) 以指示应挂起此线程以使其他等待线程能够执行。
+
+            Console.WriteLine("Main thread {0} does some work", Thread.CurrentThread.ManagedThreadId);
+
+            string returnValue = caller.EndInvoke(out threadId,result);//结束异步调用
+
+            Console.WriteLine("The call executed on thread {0},with return value \"{1}\"", threadId, returnValue);//异步操作的线程托管于线程池的
+
+            Console.ReadLine();
+        }
+    }
+}
