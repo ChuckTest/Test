2a5656164aed7200c25aad1b63c398ad602d7a69
ChuckLu <244657538@qq.com>
2014-12-10 18:19:34 +0800

    1.增加了Evolution of Delegates的代码

---
+++ CSharp编程指南/语句表达式运算符/匿名函数/EvolutionOfDelegates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvolutionOfDelegates
+{
+    /// <summary>
+    /// 定义一个委托
+    /// </summary>
+    /// <param name="s"></param>
+    delegate void TestDelegate (string s);
+
+    /// <summary>
+    /// http://msdn.microsoft.com/en-us/library/bb882516(v=vs.110).aspx
+    /// </summary>
+    class Program
+    {
+        static void M(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static void Main(string[] args)
+        {
+            // Original delegate syntax required initialization with a named method.
+            TestDelegate a = new TestDelegate(M);
+
+            //C# 2.0: A delegate can be initialized with inline code, called an "anonymous method." This method takes a string as an input parameter.
+            TestDelegate b = delegate(string s) { Console.WriteLine(s); };
+
+            // C# 3.0. A delegate can be initialized with a lambda expression. The lambda also takes a string as an input parameter (x). 
+            //The type of x is inferred by the compiler.
+            TestDelegate c = x => { Console.WriteLine(x); };
+
+            a("Hello,My Name is M and I write lines.");
+            b("That's nothing. I am anonymous method.");
+            c("I am lambda expression");
+
+            Console.ReadKey();
+
+        }
+    }
+}
