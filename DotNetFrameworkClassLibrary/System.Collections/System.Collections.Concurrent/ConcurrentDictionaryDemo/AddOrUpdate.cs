a6fa633ea32fdab61edd8c6b9b054699b73dbc19
Chuck <244657538@qq.com>
2015-01-02 16:10:25 +0800

    1.添加System.Collections.Concurrent命名空间下的ConcurrentDictionary<TKey, TValue>类的构造函数以及AddOrUpdate方法的代码示例

---
+++ DotNetFrameworkClassLibrary/System.Collections/System.Collections.Concurrent/ConcurrentDictionaryDemo/AddOrUpdate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace ConcurrentDictionaryDemo
+{
+    /// <summary>
+    /// ConcurrentDictionary<TKey, TValue>.AddOrUpdate()代码示例
+    /// </summary>
+    class AddOrUpdate
+    {
+        // Demonstrates:   使用到三个方法
+        //      ConcurrentDictionary<TKey, TValue>.AddOrUpdate()
+        //      ConcurrentDictionary<TKey, TValue>.GetOrAdd()
+        //      ConcurrentDictionary<TKey, TValue>[]
+        internal static void Test()
+        {
+            // Construct a ConcurrentDictionary
+            ConcurrentDictionary<int, int> cd = new ConcurrentDictionary<int, int>();
+
+            // Bombard the ConcurrentDictionary with 10000 competing AddOrUpdates
+            //给cd加key为1的键值对，然后更新key为1的value；最后更新为10000
+            Parallel.For(0, 10000, i => {
+                // Initial call will set cd[1] = 1.  
+                // Ensuing calls will set cd[1] = cd[1] + 1
+                cd.AddOrUpdate(1, 1, (key, oldValue) => oldValue + 1);
+            });
+
+            Console.WriteLine("After 10000 AddOrUpdates, cd[1] = {0} (should be 10000)", cd[1]);
+
+            // Should return 100, as key 2 is not yet in the dictionary
+            //得到或添加键值
+            //如果key为2存在，就返回value；否则添加(2,100)并返回新的value
+            int value = cd.GetOrAdd(2, (key) => 100);
+            Console.WriteLine("After initial GetOrAdd, cd[2] = {0} (should be 100)", value);
+
+            // Should return 100, as key 2 is already set to that value
+            //如果key为2存在，就返回value；否则添加(2,10000)并返回新的value
+            value = cd.GetOrAdd(2, 10000);
+            Console.WriteLine("After second GetOrAdd, cd[2] = {0} (should be 100)", value);
+        }
+    }
+}
---
+++ DotNetFrameworkClassLibrary/System.Collections/System.Collections.Concurrent/ConcurrentDictionaryDemo/Constructor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ConcurrentDictionaryDemo
+{
+    /// <summary>
+    /// 构造函数以及访问元素的代码示例
+    /// </summary>
+    class Constructor
+    {
+        //此示例仅仅使用了初始化的时候的初始元素个数
+        // Demonstrates: 示例
+        //      public ConcurrentDictionary(int concurrencyLevel,int capacity)  //构造函数
+        //      参数说明
+        //      concurrencyLevel  //将要并发更新ConcurrentDictionary<TKey, TValue>的预估线程的数量
+        //      The estimated number of threads that will update the ConcurrentDictionary<TKey, TValue> concurrently.
+        //      capacity  //ConcurrentDictionary<TKey, TValue>所能包含的初始元素个数
+        //      The initial number of elements that the ConcurrentDictionary<TKey, TValue> can contain.
+        //
+        //      ConcurrentDictionary<TKey, TValue>[TKey]    //访问ConcurrentDictionary类中的元素
+        internal static void Test()
+        {
+            Console.WriteLine("构造函数测试开始"); 
+            // We know how many items we want to insert into the ConcurrentDictionary.
+            // 已经知道想要插多少元素到ConcurrentDictionary
+            // So set the initial capacity to some prime number above that, to ensure that
+            // the ConcurrentDictionary does not need to be resized while initializing it.
+            // 设置初始的允许的元素个数大于已经要插入的元素个数，确保ConcurrentDictionary在初始化的时候，不需要调整大小
+            int NUMITEMS = 64;
+            int initialCapacity = 101;
+
+            //concurrency并发  theoretical理论上的
+            // The higher the concurrencyLevel, the higher the theoretical number of operations
+            // that could be performed concurrently on the ConcurrentDictionary.  However, global
+            // operations like resizing the dictionary take longer as the concurrencyLevel rises. 
+            // For the purposes of this example, we'll compromise at numCores * 2.
+            //越高的并发等级，理论上就会有越高的处理次数
+            int numProcs = Environment.ProcessorCount;
+            int concurrencyLevel = numProcs * 2;
+
+            // Construct the dictionary with the desired concurrencyLevel and initialCapacity
+            //public ConcurrentDictionary(int concurrencyLevel,int capacity)
+            ConcurrentDictionary<int, int> cd = new ConcurrentDictionary<int, int>(concurrencyLevel, initialCapacity);
+
+            // Initialize the dictionary    初始化并发字典  数量为64，小于初始化提供的基本参数101
+            for (int i = 0; i < NUMITEMS; i++)
+            {
+                cd[i] = i * i;//ConcurrentDictionary<TKey, TValue>[TKey] 
+            }
+
+            Console.WriteLine("The square of 23 is {0} (should be {1})", cd[23], 23 * 23);
+
+            Console.WriteLine("构造函数测试结束"); 
+        }
+    }
+}
