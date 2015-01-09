f00b4dda4fa3b0b22b5f45a6612cec8feb282bd8
ChuckLu <244657538@qq.com>
2015-01-09 21:07:59 +0800

    1.增加《C#入门经典》第10章的扑克牌的代码

---
+++ CSharpBegin/Part1/Chapter10/Ch10CardLib/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch10CardLib
+{
+    public class Card
+    {
+        private readonly Rank rank;
+        private readonly Suit suit;
+
+        private Card()
+        {
+            
+        }
+
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("The {0} of {1}s", rank, suit);
+        }
+    }
+}
