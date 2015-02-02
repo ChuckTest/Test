960456b276d32c4c36df43dd91bacedd95ee8a3b
ChuckLu <244657538@qq.com>
2015-02-02 21:22:40 +0800

    1.添加进制转换的demo

---
+++ Demo/Basic/ConvertHelper/ConvertHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertHelperDemo
+{
+    public class ConvertHelper
+    {
+        /// <summary>
+        /// 十六进制字符串转换成字节数组  字符串长度必须为偶数
+        /// 举例   输入abcdef0103dac3  不区分大小写
+        /// 得到的字节数组  171,205,239,  1,  3,218,195,
+        /// 171对应ab，205对应cd，239对应ef，01对应1,03对应3，da对应218，c3对应195
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static byte[] HexStringToByteArray(string s)
+        {
+            try
+            {
+                Debug.WriteLine(string.Format("字符串{0}的长度为{1}", s, s.Length));
+
+                if (s.Length % 2 == 1)
+                {
+                    throw new Exception(string.Format("HexStringToByteArray传入的字符串长度为奇数{0}，不符合要求", s.Length));
+                }
+
+                byte[] buffer = new byte[s.Length / 2];
+                string tmp = string.Empty;
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    //从字符串中逐次截取2个长度的子字符串,
+                    tmp = s.Substring(i * 2, 2);
+                    buffer[i] = Convert.ToByte(tmp, 16);//第二个参数16表示tmp子字符串中是十六进制
+                }
+                return buffer;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Demo/Basic/ConvertHelper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertHelperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //string hexString = "Abcdef0103dac3";
+                //byte[] array = ConvertHelper.HexStringToByteArray(hexString);
+                //for (int i = 0; i < array.Length; i++)
+                //{
+                //    Console.Write("{0,3},",array[i]);
+                //}
+
+
+
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("捕获异常信息：{0}", ex.Message));
+            }
+            Console.ReadKey();
+        }
+    }
+}
