381c06c09bf36d040a543a6e651874e645bf8238
ChuckLu <244657538@qq.com>
2015-03-03 20:34:26 +0800

    1.添加Ch11Ex02的代码，定义集合以及索引符的使用

---
+++ CSharpBegin/Part1/Chapter11/Ch11Ex02/Animals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11Ex02
+{
+    class Animals : CollectionBase
+    {
+        public void Add(Animal newAnimal)
+        {
+            List.Add(newAnimal);
+        }
+
+        public void Remove(Animal oldAnimal)
+        {
+            List.Remove(oldAnimal);
+        }
+
+        public Animals()
+        { }
+
+        //索引符是一种特殊类型的属性，可以把它添加到一个类中，以提供类似于数组的访问
+        public Animal this[int animalIndex]
+        {
+            get 
+            {
+                //List属性返回的是object对象，所以需要强制转换
+                return (Animal)List[animalIndex];
+            }
+            set 
+            { 
+                List[animalIndex] = value; 
+            }
+        }
+
+    }
+}
---
+++ CSharpBegin/Part1/Chapter11/Ch11Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals animalCollection = new Animals();
+            animalCollection.Add(new Cow("Jack"));
+            animalCollection.Add(new Chicken("Vera"));
+            foreach (Animal item in animalCollection)
+            {
+                item.Feed();
+            }
+            Console.ReadKey();
+        }
+    }
+}
