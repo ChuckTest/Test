e28ceb23ff04d5b2de150b5b21af8dea8e8a31be
ChuckLu <244657538@qq.com>
2015-04-04 14:32:41 +0800

    1.使用yield关键字构建SimpleIterators

---
+++ CSharpBegin/Part1/Chapter11/SimpleIterators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace SimpleIterators
+{
+   
+    class Program
+    {
+        static IEnumerable SimpleList()
+        {
+            yield return "string 1";
+            yield return "string 2";
+            yield return "string 3";
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (string item in SimpleList())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
