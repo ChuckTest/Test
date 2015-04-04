291809d5b650a51c91726ceb9cb897d18588eb4e
ChuckLu <244657538@qq.com>
2015-04-04 19:59:56 +0800

    1.使用yield关键字，foreach进行遍历操作，CH11Ex03项目

---
+++ CSharpBegin/Part1/Chapter11/Ch11Ex03/Primes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Ch11Ex03
+{
+    class Primes
+    {
+        private long min;
+        private long max;
+
+        public Primes(long minimum, long maximum)
+        {
+            if (min < 2)
+            {
+                min = 2;
+            }
+            else
+            {
+                min = minimum;
+            }
+            max = maximum;
+        }
+
+        public Primes()
+            : this(2, 100)
+        { }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (long posssiblePrime = min; posssiblePrime <= max; posssiblePrime++)
+            {
+                bool isPrime = true;
+                long tmpMax=(long)Math.Floor(Math.Sqrt(posssiblePrime));
+                for (long possibleFactor = 2; possibleFactor <= tmpMax; possibleFactor++)
+                {
+                    long remainderAfterDivision = posssiblePrime % possibleFactor;
+                    if (remainderAfterDivision == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    yield return posssiblePrime;
+                }
+            }
+        }
+    }
+}
---
+++ CSharpBegin/Part1/Chapter11/Ch11Ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primes primes2To1000 = new Primes(2, 1000);
+            foreach (long i in primes2To1000)
+            {
+                Console.Write("{0}  ",i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
