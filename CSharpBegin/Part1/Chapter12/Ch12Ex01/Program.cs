2e8ccffc1f619b1cbbe555164f6f6526d2c9ac68
ChuckLu <244657538@qq.com>
2015-04-20 17:07:50 +0800

    完善了Vector的代码

--- CSharpBegin/Part1/Chapter12/Ch12Ex01/Program.cs
+++ CSharpBegin/Part1/Chapter12/Ch12Ex01/Program.cs
@@ -8,8 +8,37 @@ namespace Ch12Ex01
 {
     class Program
     {
+        public static double? GetNullableDouble()
+        {
+            double? result;
+            string userInput = Console.ReadLine();
+            try
+            {
+                result = double.Parse(userInput);
+            }
+            catch
+            {
+                result = null;
+            }
+            return result;
+        }
+
+        public static Vector GetVector(string name)
+        {
+            Console.WriteLine("Input {0} magnitude:", name);
+            double? r = GetNullableDouble();
+            Console.WriteLine("Input {0} angle (in degrees):", name);
+            double? theta = GetNullableDouble();
+            return new Vector(r, theta);
+        }
+
         static void Main(string[] args)
         {
+            Vector v1 = GetVector("vector1"); 
+            Vector v2 = GetVector("vector2");
+            Console.WriteLine("{0} + {1} = {2}", v1, v2, v1 + v2);
+            Console.WriteLine("{0} - {1} = {2}", v1, v2, v1 - v2);
+            Console.ReadKey();
         }
     }
 }
--- CSharpBegin/Part1/Chapter12/Ch12Ex01/Vector.cs
+++ CSharpBegin/Part1/Chapter12/Ch12Ex01/Vector.cs
@@ -6,19 +6,42 @@ using System.Threading.Tasks;
 
 namespace Ch12Ex01
 {
+    /// <summary>
+    /// 表示带极坐标的矢量
+    /// </summary>
     class Vector
     {
+        /// <summary>
+        /// 极径
+        /// </summary>
         public double? R = null;
+        /// <summary>
+        /// 极角(单位：度)  逆时针方向为正
+        /// </summary>
         public double? Theta = null;
 
+        /// <summary>
+        /// 弧度 弧长等于半径的弧，其所对的圆心角为1弧度
+        /// 一周对应的弧度 2Πr/r=2Π  
+        /// </summary>
         public double? ThetaRadians//弧度
         {
-            //把角度转换为弧度
-            get { return (Theta * Math.PI / 180.0); }
+            get
+            {
+                //把角度转换为弧度   180°对应于Π
+                return Theta / 180.0 * Math.PI; 
+            }
         }
 
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="r">极径</param>
+        /// <param name="theta">极角</param>
         public Vector(double? r, double? theta)
         {
+            //坐标(3,60°)和(-3,240°)是同一个点
+            //(-3,240°)是240°方向的反向延长线上的3个单位长度
             if (r < 0)
             {
                 r = -r;
@@ -30,10 +53,19 @@ namespace Ch12Ex01
             Theta = theta;
         }
 
+        /// <summary>
+        /// 重载加法运算符
+        /// </summary>
+        /// <param name="op1"></param>
+        /// <param name="op2"></param>
+        /// <returns></returns>
         public static Vector operator +(Vector op1, Vector op2)
         {
+            //C#入门经典中的极坐标，以纵轴为极轴，顺时针方向为正的极角
+            //所以纵向分解是R*Cos(theta)  横向分解是R*Sin(theta)  
             try
             {
+                //获取新矢量的(x,y)坐标
                 double newX = op1.R.Value * Math.Sin(op1.ThetaRadians.Value) + op2.R.Value * Math.Sin(op2.ThetaRadians.Value);
                 double newY = op1.R.Value * Math.Cos(op1.ThetaRadians.Value) + op2.R.Value * Math.Cos(op2.ThetaRadians.Value);
 
@@ -48,21 +80,36 @@ namespace Ch12Ex01
             }
         }
 
+        /// <summary>
+        /// 重载负号运算符
+        /// </summary>
+        /// <param name="op1"></param>
+        /// <returns></returns>
         public static Vector operator -(Vector op1)
         {
             return new Vector(-op1.R, op1.Theta);
         }
 
+        /// <summary>
+        /// 重载减法运算符
+        /// </summary>
+        /// <param name="op1"></param>
+        /// <param name="op2"></param>
+        /// <returns></returns>
         public static Vector operator -(Vector op1, Vector op2)
         {
             return op1 + (-op2);
         }
 
+        /// <summary>
+        /// 重写Object的ToString方法
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             string rString = R.HasValue ? R.ToString() : "null";
             string thetaString = Theta.HasValue ? Theta.ToString() : "null";
-            return string.Format("{0},{1}", rString, thetaString);
+            return string.Format("({0},{1})", rString, thetaString);
         }
     }
 }
