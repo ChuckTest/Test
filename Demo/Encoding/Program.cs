42a44cb3017d143080164baef0b2d39d35c049da
Chuck Lu <244657538@qq.com>
2017-07-13 11:16:20 +0800

    test the length of byte array

--- Demo/Encoding/Program.cs
+++ Demo/Encoding/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace ConsoleApplicationEncoding
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace ConsoleApplicationEncoding
 {
-    class Program
+    [TestFixture]
+    public class EncodingTest
     {
-        static void Main(string[] args)
+
+        [Test]
+        public void Test()
         {
+            List<string> list = new List<string> {"hello", "你好", "hello你好"};
+
+            foreach (var item in list)
+            {
+                Output(item);
+            }
+        }
+
+        private void Output(string input)
+        {
+            OutputLengthOfByteArray(input, Encoding.ASCII);
+            OutputLengthOfByteArray(input, Encoding.Unicode);
+            OutputLengthOfByteArray(input, Encoding.UTF32);
+            OutputLengthOfByteArray(input, Encoding.UTF8);
+            OutputLengthOfByteArray(input, Encoding.UTF7);
+            Console.WriteLine();
+        }
+
+        public void OutputLengthOfByteArray(string input, Encoding encoding)
+        {
+            var array = encoding.GetBytes(input);
+            Console.WriteLine($"Length of byte array for {input} in {encoding} is {array.Length}");
         }
     }
 }
